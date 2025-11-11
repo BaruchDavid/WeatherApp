2b108c89d908a9d68d8708ce46b6a16007663d96
Baruch David <kaproma@yahoo.de>
2025-11-11 18:01:12 +0100

    remove comments

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,10 @@ import { WeatherComponent } from "./weather/weather.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,   // ← unbedingt hinzufügen
+  standalone: true,
   imports: [RouterOutlet, WeatherComponent],
   templateUrl: './app.html',
-  styleUrls: ['./app.css'] // Achtung: `styleUrls`, nicht `styleUrl`
+  styleUrls: ['./app.css'] 
 })
 export class App {
   protected readonly title = signal('weather-app');
